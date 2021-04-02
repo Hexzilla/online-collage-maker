953d45b4e6d043bda04faaa86ecee4d2afcb1309
rightvalue <bluebird6922@gmail.com>
2021-04-02 18:14:56 +0900

    0402/Update-1

--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -3,7 +3,6 @@ import { ToastrService } from "ngx-toastr";
 import { ApiService } from "../api/api";
 import { ImageService } from "../collage/image.service";
 import { Setting } from "../collage/setting";
-import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: "image-upload",
@@ -12,16 +11,20 @@ import { HttpEventType } from '@angular/common/http';
 })
 export class ImageUploadComponent implements OnInit {
   files: File[] = [];
-  uploadProgress = -1;
-  uploadButtonName = "Upload"
+  progressShow: boolean;
+  dropZoneStatus: boolean;
   @Input() imageSvc: ImageService
 
   constructor(
     private toastr: ToastrService,
+    private api: ApiService,
     public  setting: Setting
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.progressShow = false
+    this.dropZoneStatus = false
+  }
 
   onSelect(event) {
     this.files.push(...event.addedFiles)
@@ -39,35 +42,25 @@ export class ImageUploadComponent implements OnInit {
       this.toastr.error("Please select file")
       return;
     }
-
-    this.uploadProgress = 0;
-    this.uploadButtonName = `Image uploading : ${this.uploadProgress} % `;
-
+    
+    this.progressShow = true;
+    this.dropZoneStatus = true;
     let formData = new FormData();
     for (var i = 0; i < this.files.length; i++) {
       formData.append("images", this.files[i], this.files[i].name)
     }
 
-    this.imageSvc.uploadCollageImages(formData).subscribe((event: any) => {
-      console.log("event.type: ", event.type);
-      if (event.type === HttpEventType.UploadProgress) {
-        const uploadProgress = Math.round((event.loaded / event.total * 100));
-        this.uploadProgress = uploadProgress;
-        this.uploadButtonName = `Image uploading : ${uploadProgress} % `;
-      } 
-      else if (event.type === HttpEventType.Response) {
-        this.files = []
-        this.reset()
-      }
-    }, err => {
-      console.log(err.message);
-      this.reset()
-      this.toastr.error("Failed to upload images")
-    });
-  }
+    const result = await this.imageSvc.uploadImage(formData)
+    if (result) {
+      this.toastr.success("success")
+      await this.imageSvc.updateImages()
+    } 
+    else {
+      this.toastr.error("failed")
+    }
 
-  reset() {
-    this.uploadProgress = -1;
-    this.uploadButtonName = "Upload";
+    this.files = []
+    this.progressShow = false
+    this.dropZoneStatus = false
   }
 }
